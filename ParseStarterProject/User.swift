89b05d83bf464906110384fcf4bb1203d2bedf47
Shumin Gao <shumin2014@gmail.com>
2015-08-15 21:51:32 -0700

    add user model

---
+++ ParseStarterProject/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Shumin Gao on 8/15/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    var displayName: String?
+    var email: String?
+    var password: String?
+    
+}
