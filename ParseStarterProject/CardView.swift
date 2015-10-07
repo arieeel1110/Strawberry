8763770e16c9c84e0190e78ff1aaf0d072c80948
arieeel <arieeel1110@gmail.com>
2015-10-06 22:16:29 -0700

    cardView

--- ParseStarterProject/CardView.swift
+++ ParseStarterProject/CardView.swift
@@ -16,7 +16,9 @@ class CardView: MDCSwipeToChooseView {
     var informationView: UIView!
     var nameLabel: UILabel!
     var authorLabel: UILabel!
+    var likeLabel: UILabel!
     var carmeraImageLabelView:ImagelabelView!
+    var starView:ImagelabelView!
     
     init(frame: CGRect, post: Post, options: MDCSwipeToChooseViewOptions) {
         
@@ -74,7 +76,9 @@ class CardView: MDCSwipeToChooseView {
     }
     
     func constructAuthorLabel() -> Void{
-        var leftPadding:CGFloat = 37.0;
+
+        
+        var leftPadding:CGFloat = 40.0;
         var topPadding:CGFloat = self.informationView.frame.maxY - 59
         
         var frame:CGRect = CGRectMake(leftPadding,
@@ -82,22 +86,46 @@ class CardView: MDCSwipeToChooseView {
             floor(CGRectGetWidth(self.informationView.frame)/2+150.0),
             CGRectGetHeight(self.informationView.frame) + 50)
         self.authorLabel = UILabel(frame:frame)
-        self.authorLabel.text = "\(post.Author)" + " , " + "\(post.Like)" + " likes"
+        self.authorLabel.text = "\(post.Author)"
         
         self.authorLabel.font = UIFont(name: "HelveticaNeue-Bold", size: CGFloat(20))
         self.authorLabel.layer.shadowOpacity = 0.7
         self.authorLabel.textColor = UIColor.whiteColor()
         
         insertSubview(self.authorLabel,aboveSubview:self.informationView)
+        
+        var image:UIImage = UIImage(named:"star")!
+        
+        var frame2:CGRect = CGRect(x:6.0, y: 210.5,
+            width: 25,
+            height:25)
+        self.starView = ImagelabelView(frame:frame2, image:image, text:self.authorLabel.text!)
+        
+        insertSubview(self.starView,aboveSubview:self.informationView)
     }
     
     func constructCameraImageLabelView() -> Void{
-        var image:UIImage = UIImage(named:"star")!
         
-        var frame:CGRect = CGRect(x:5.0, y: 210.5,
+        var frame:CGRect = CGRectMake(228.0,
+            self.informationView.frame.maxY - 59,
+            floor(CGRectGetWidth(self.informationView.frame)/2+150.0),
+            CGRectGetHeight(self.informationView.frame) + 50)
+        self.likeLabel = UILabel(frame:frame)
+        self.likeLabel.text = "\(post.Like)" + " likes"
+        
+        self.likeLabel.font = UIFont(name: "HelveticaNeue-Bold", size: CGFloat(20))
+        self.likeLabel.layer.shadowOpacity = 0.7
+        self.likeLabel.textColor = UIColor.whiteColor()
+        
+        insertSubview(self.likeLabel,aboveSubview:self.informationView)
+        
+        
+        var image:UIImage = UIImage(named:"favourite")!
+        
+        var frame2:CGRect = CGRect(x:100.0, y: 210.5,
             width: 25,
             height:25)
-        self.carmeraImageLabelView = ImagelabelView(frame:frame, image:image, text:self.authorLabel.text!)
+        self.carmeraImageLabelView = ImagelabelView(frame:frame2, image:image, text:self.authorLabel.text!)
         
         insertSubview(self.carmeraImageLabelView,aboveSubview:self.informationView)
     }
